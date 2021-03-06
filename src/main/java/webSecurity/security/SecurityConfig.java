284a7e1c112a534a2eba111c33354de50b5c7d31
Zortela <71187449+Zortela@users.noreply.github.com>
2021-03-06 14:40:17 +0300

    Update SecurityConfig.java

--- src/main/java/webSecurity/security/SecurityConfig.java
+++ src/main/java/webSecurity/security/SecurityConfig.java
@@ -20,57 +20,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @ComponentScan("webSecurity")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    /*@Autowired
-    private UserDetailsService userDetailsService;
-
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
-                // указываем страницу с формой логина
-                .loginPage("/login")
-                //указываем логику обработки при логине
-                .successHandler(new LoginSuccessHandler())
-                // указываем action с формы логина
-                .loginProcessingUrl("/login")
-                // даем доступ к форме логина всем
-                .permitAll();
-
-        http.logout()
-                // разрешаем делать логаут всем
-                .permitAll()
-                // указываем URL логаута
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                // указываем URL при удачном логауте
-                .logoutSuccessUrl("/login?logout")
-                //выклчаем кроссдоменную секьюрность (на этапе обучения неважна)
-                .and().csrf().disable();
-
-        http
-                .authorizeRequests()
-                .antMatchers("/login").anonymous()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/user/**").hasAnyRole("ADMIN", "USER")
-                .anyRequest().authenticated();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }*/
-
-
-    /*@Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("ADMIN")
-                .password(passwordEncoder().encode("ADMIN"))
-                .roles("ADMIN");
-    }*/
 
     private final UserDetailsService userDetailsService;
 
